0f5b2062cd57f28a6fc4d4eef6e99871f2b3256d
Jorge Leao <jorge.leao@ufrj.br>
2022-03-28 12:07:35 -0300

    new structure

--- sortingA/src/main/java/com/intelie/ReorderJsonArray.java
+++ sortingA/src/main/java/com/intelie/ReorderJsonArray.java
@@ -1,9 +1,11 @@
 package com.intelie;
 
 import com.google.gson.Gson;
+
 import java.util.Locale;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
+
 import static java.util.Arrays.stream;
 
 public class ReorderJsonArray {
@@ -21,9 +23,9 @@ public class ReorderJsonArray {
      */
     public String byOneKey(String jsonString, String key) {
 
-        TreeMap<String, OneKeyObjectsJsonArray> treeMap = new TreeMap<>();
-        OneKeyObjectsJsonArray[] oneKeyJsonObjectsInput =
-                gson.fromJson(jsonString, OneKeyObjectsJsonArray[].class);
+        TreeMap<String, TwoKeyObjectsJsonArray> treeMap = new TreeMap<>();
+        TwoKeyObjectsJsonArray[] oneKeyJsonObjectsInput =
+                gson.fromJson(jsonString, TwoKeyObjectsJsonArray[].class);
 
         stream(oneKeyJsonObjectsInput)
                 .map(x -> {
@@ -40,8 +42,8 @@ public class ReorderJsonArray {
                 })
                 .collect(Collectors.toList());
 
-        OneKeyObjectsJsonArray[] oneKeyJsonObjectsOutput =
-                new OneKeyObjectsJsonArray[oneKeyJsonObjectsInput.length];
+        TwoKeyObjectsJsonArray[] oneKeyJsonObjectsOutput =
+                new TwoKeyObjectsJsonArray[oneKeyJsonObjectsInput.length];
         int i = 0;
         for (String s : treeMap.keySet()) {
             oneKeyJsonObjectsOutput[i] = treeMap.get(s);
@@ -52,7 +54,7 @@ public class ReorderJsonArray {
     }
 }
 
-class OneKeyObjectsJsonArray {
+class TwoKeyObjectsJsonArray {
     private String aKey;
     private String zzz;
 
