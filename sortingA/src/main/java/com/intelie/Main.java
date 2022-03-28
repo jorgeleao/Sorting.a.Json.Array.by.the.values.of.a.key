79f366b25e3f6e2993356741210eb71e4f18d797
Jorge Leao <jorge.leao@ufrj.br>
2022-03-28 11:05:43 -0300

    new structure

--- sortingA/src/main/java/com/intelie/Main.java
+++ sortingA/src/main/java/com/intelie/Main.java
@@ -1,15 +1,11 @@
 package com.intelie;
 
 import com.google.gson.Gson;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Locale;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
-
-import static java.util.Arrays.stream;
 
 public class Main {
 
@@ -18,34 +14,28 @@ public class Main {
         Gson gson = new Gson();
         TwoKeyJsonObject[] twoKeyJsonObjectsEntrada = null;
         TwoKeyJsonObject[] twoKeyJsonObjectsSaida = null;
+        String jsonString = null;
         String saida = null;
 
         try (Reader reader = new FileReader("jsonArray.json")) {
             twoKeyJsonObjectsEntrada = gson.fromJson(reader, TwoKeyJsonObject[].class);
-            TreeMap<String, TwoKeyJsonObject> firstMap = new TreeMap<>();
-            stream(twoKeyJsonObjectsEntrada)
-                    .map(x -> { String str = x.getaKey()
-                                              .trim()
-                                              .toUpperCase(Locale.ROOT);
-                                str = RemoveDiacriticals.remove(str);
-                                firstMap.put(str,x);
-                                return null;})
-                    .collect(Collectors.toList());
+            jsonString = gson.toJson(twoKeyJsonObjectsEntrada);
+            System.out.println(jsonString);
 
-            twoKeyJsonObjectsSaida = new TwoKeyJsonObject[twoKeyJsonObjectsEntrada.length];
-            int i = 0;
-            for (String s : firstMap.keySet()) {
-                System.out.println(s);
-                twoKeyJsonObjectsSaida[i] = firstMap.get(s);
-                i++;
-            }
-            saida = gson.toJson(twoKeyJsonObjectsSaida);
+//            File file = new File("jsonArray.json");
+//            jsonString = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+//            System.out.println(jsonString);
+
+            //jsonString = gson.fromJson(reader, TwoKeyJsonObject[].class).toString();
+
+
+            saida = new ReorderJsonArray().byOneKey(jsonString, "aKey");
             System.out.println(saida);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try (FileWriter writer = new FileWriter("jsonArraySaida.json")){
+        try (FileWriter writer = new FileWriter("jsonArraySaida.json")) {
             gson.toJson(twoKeyJsonObjectsSaida, writer);
         } catch (IOException e) {
             e.printStackTrace();
---
+++ sortingA/src/main/java/com/intelie/ReorderJsonArray.java
@@ -0,0 +1,64 @@
+package com.intelie;
+
+import com.google.gson.Gson;
+
+import java.util.Locale;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+public class ReorderJsonArray {
+    Gson gson = new Gson();
+
+    /**
+     * TODO:
+     * - Must accept repeated key values
+     * - Must process json objects of arbitrary structure/content
+     * - Must include objects without the reference key at the end
+     * @param jsonString
+     * @param key
+     * @return
+     */
+    public String byOneKey(String jsonString, String key) {
+
+        TreeMap<String, OneKeyObjectsJsonArray> treeMap = new TreeMap<>();
+        OneKeyObjectsJsonArray[] oneKeyJsonObjectsInput =
+                gson.fromJson(jsonString, OneKeyObjectsJsonArray[].class);
+
+        stream(oneKeyJsonObjectsInput)
+                .map(x -> {
+                    if(x.getaKey()==null){
+                        treeMap.put("zzzzzzzzzzzz", x);
+                        return null;
+                    }else {
+                        String str = x.getaKey()
+                                .trim()
+                                .toUpperCase(Locale.ROOT);
+                        str = RemoveDiacriticals.remove(str);
+                        treeMap.put(str, x);
+                        return null;
+                    }
+                })
+                .collect(Collectors.toList());
+
+        OneKeyObjectsJsonArray[] oneKeyJsonObjectsOutput =
+                new OneKeyObjectsJsonArray[oneKeyJsonObjectsInput.length];
+        int i = 0;
+        for (String s : treeMap.keySet()) {
+            oneKeyJsonObjectsOutput[i] = treeMap.get(s);
+            i++;
+        }
+
+        return gson.toJson(oneKeyJsonObjectsOutput);
+    }
+}
+
+class OneKeyObjectsJsonArray {
+    private String aKey;
+    private String zzz;
+
+    public String getaKey() {
+        return aKey;
+    }
+}
